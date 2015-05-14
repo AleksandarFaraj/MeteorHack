5bc68e5e0b67033c3eb7faff8b52ac08372e9a0f
Aleksandar Faraj <aleksandarfarajgmail.com>
2015-05-14 19:26:57 +0200

    added search button

--- client/controller/AppController.js
+++ client/controller/AppController.js
@@ -2,23 +2,26 @@ angular
     .module('snapify')
     .controller('AppController', AppController);
 
-AppController.$inject = ['$scope','$meteor'];
+AppController.$inject = ['$scope', '$meteor'];
 
 /* @ngInject */
-function AppController($scope,$meteor) {
+function AppController($scope, $meteor) {
     /* jshint validthis: true */
     var vm = this;
     vm.title = 'AppController';
 
     ////////////////
     console.log('hello world');
-    $scope.login = function() {
-        $meteor.loginWithFacebook({},function(){
+    $scope.login = function () {
+        $meteor.loginWithFacebook({}, function () {
             console.log('Logged in');
         });
     };
-    $scope.setUsername = function(username) {
-        $meteor.call('setUsername',username);
+    $scope.setUsername = function (username) {
+        $meteor.call('setUsername', username);
     };
+    $scope.friends = $meteor.collection(function () {
+        return Meteor.users.find({});
+    });
 
 }
